efc3d043ec7e36d8c5a981edc19d336c53b3aa37
KyAMogollon <adhair.mogollon@tecsup.edu.pe>
2023-05-09 12:26:01 -0500

    Update

--- Assets/Assets/Scripts/PlayerController.cs
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,29 +13,48 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private GameObject balaPref;
     [SerializeField] private Transform posicionDisparo;
+    Vector2 movementPlayer;
+    Vector2 mouseInput;
 
     private void Start()
     {
     }
     private void Update() {
-        Vector2 movementPlayer = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        
         myRBD2.velocity = movementPlayer * velocityModifier;
 
         animatorController.SetVelocity(velocityCharacter: myRBD2.velocity.magnitude);
-        Vector2 mouseInput = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
+        //mouseInput = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //mouseInput = mouseInput - transform.position;
         CheckFlip(mouseInput.x);
     
         Debug.DrawRay(transform.position, mouseInput.normalized * rayDistance, Color.red);
 
-        if(Input.GetMouseButtonDown(0)){
+        /*if(Input.GetMouseButtonDown(0)){
             Instantiate(balaPref, transform.position, Quaternion.identity);
         }else if(Input.GetMouseButtonDown(1)){
             Instantiate(balaPref, transform.position, Quaternion.identity);
-        }
+        }*/
     }
 
     private void CheckFlip(float x_Position){
         spriteRenderer.flipX = (x_Position - transform.position.x) < 0;
+    }
+    public void OnMovement(InputAction.CallbackContext value)
+    {
+        Vector2 inputMovement = value.ReadValue<Vector2>();
+        movementPlayer = new Vector2(inputMovement.x, inputMovement.y);
+    }
+    public void OnAim(InputAction.CallbackContext value)
+    {
+        Vector2 inputMouse = Camera.main.ScreenToWorldPoint(value.ReadValue<Vector2>());
+        mouseInput = inputMouse;
+    }
+    public void OnFire(InputAction.CallbackContext value)
+    {
+        if (value.started) {
+            Vector2 disparo = new Vector2((mouseInput.x - transform.position.x), (mouseInput.y - transform.position.y));
+            Instantiate(balaPref, disparo, Quaternion.identity);
+        }
     }
 }
